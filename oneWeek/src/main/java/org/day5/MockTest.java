29ce8404e341cfce6e7733c20dc3c5d30a52bfa3
YounSoyoung <grace902853@gmail.com>
2023-06-12 18:20:58 +0900

    Mock Test

---
+++ oneWeek/src/main/java/org/day5/MockTest.java
@@ -0,0 +1,71 @@
+package org.day5;
+
+
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+        import static java.util.stream.Collectors.toList;
+
+
+class TestResult {
+
+    /*
+     * Complete the 'pairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int pairs(int k, List<Integer> arr) {
+        // Write your code here
+        Collections.sort(arr); //1 2 3 4 5
+
+        int answer = 0;
+
+        for(int i = 0; i < arr.size(); i++){
+            for(int j = i + 1; j < arr.size(); j++){
+                if(arr.get(i) + k == arr.get(j)){
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+
+    }
+
+}
+public class MockTest {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = TestResult.pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
