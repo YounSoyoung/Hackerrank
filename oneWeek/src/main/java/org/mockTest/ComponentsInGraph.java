c4fd28db75a3c743c6e140da14f36d6a2ed958ad
YounSoyoung <grace902853@gmail.com>
2023-06-16 17:30:01 +0900

    java practice

---
+++ oneWeek/src/main/java/org/mockTest/ComponentsInGraph.java
@@ -0,0 +1,67 @@
+package org.mockTest;
+
+import java.io.*;
+        import java.math.*;
+        import java.security.*;
+        import java.text.*;
+        import java.util.*;
+        import java.util.concurrent.*;
+        import java.util.function.*;
+        import java.util.regex.*;
+        import java.util.stream.*;
+        import static java.util.stream.Collectors.joining;
+        import static java.util.stream.Collectors.toList;
+
+
+
+class ComponentsResult {
+
+    /*
+     * Complete the 'componentsInGraph' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts 2D_INTEGER_ARRAY gb as parameter.
+     */
+
+    public static List<Integer> componentsInGraph(List<List<Integer>> gb) {
+        // Write your code here
+
+    }
+
+}
+
+public class ComponentsInGraph {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> gb = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                gb.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = ComponentsResult.componentsInGraph(gb);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
